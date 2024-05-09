76a7165abf613dfc7f3f1debf91a0655552ab150
Adrian Kühni <adrian.kuehni@gmail.com>
2024-05-09 21:21:41 +0200

    feat: integrate Google Maps API

--- lib/map.dart
+++ lib/map.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:mad_location_tracker/app_bar.dart';
 
 class MapView extends StatelessWidget {
@@ -6,18 +9,79 @@ class MapView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return const MapSample();
+  }
+}
+
+class MapSample extends StatefulWidget {
+  const MapSample({super.key});
+
+  @override
+  State<MapSample> createState() => MapSampleState();
+}
+
+class MapSampleState extends State<MapSample> {
+  final _controller = Completer<GoogleMapController>();
+  var _pos = 0;
+
+  static const _positions = {
+    "Informaticon": LatLng(46.7559, 7.6149),
+    "Bahnhof": LatLng(46.7548, 7.6297),
+    "Schloss": LatLng(46.7598, 7.6299),
+  };
+  static final Set<Marker> _markers =
+  Set.from(_positions.entries.map((entry) =>
+      Marker(
+        markerId: MarkerId(entry.key),
+        infoWindow: InfoWindow(title: entry.key),
+        position: entry.value,
+      )));
+
+  @override
+  Widget build(BuildContext context) {
+    var firstPosition = _positions.values.first;
+
     return Scaffold(
       appBar: getAppBar(context),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'map view',
-            )
-          ],
-        ),
+      body: Stack(
+        children: [
+          GoogleMap(
+            mapType: MapType.hybrid,
+            initialCameraPosition: CameraPosition(
+              target: firstPosition,
+              zoom: 19,
+            ),
+            onMapCreated: (controller) {
+              _controller.complete(controller);
+            },
+            markers: _markers,
+            compassEnabled: true,
+            myLocationEnabled: true,
+            myLocationButtonEnabled: true,
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: ElevatedButton(
+                onPressed: _nextPos,
+                child: const Text("Next"),
+              ),
+            ),
+          ),
+        ],
       ),
     );
+  }
+
+  _nextPos() async {
+    _pos += 1;
+    var next = CameraPosition(
+      target: List.from(_positions.values)[_pos % _positions.length]!,
+      zoom: 20,
+    );
+
+    final controller = await _controller.future;
+    await controller.animateCamera(CameraUpdate.newCameraPosition(next));
   }
 }
